980a74f85fbc6978f638a0877a2b2465bdd733aa
Dev Hans <helginura@gmail.com>
2025-09-24 17:06:27 +0700

    Refactor CategoryFactory to define model attributes for name, slug, and description

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
@@ -16,8 +16,11 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        $name = fake()->unique()->words(2, true);
         return [
-            //
+            'name'        => $name,
+            'slug'        => Str::slug($name),
+            'description' => fake()->sentence(),
         ];
     }
 }
